f346e8d3c52593218343f90e30acf2816dc0b8fb
hashZero1 <117970097+hashZero1@users.noreply.github.com>
2023-05-10 12:13:24 +0530

    configure loader

--- src/JokeCollection.js
+++ src/JokeCollection.js
@@ -3,15 +3,15 @@ import Joke from './Joke';
 import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
+import Loader from './Loader';
 import uuid from 'react-uuid';
-
 class JokeCollection extends Component {
     static defaultProps = {
         numJoke: 10
     };
     constructor(props){
         super(props);
-        this.state = {jokes: []};
+        this.state = {jokes: [] , isLoading: false};
     }
 
     async componentDidMount(){
@@ -19,16 +19,21 @@ class JokeCollection extends Component {
         while(joke.length < this.props.numJoke){
         const config = { headers: { Accept: 'application/json' } };
         let response =  await axios.get('https://icanhazdadjoke.com/', config);
-
          //put keys into new object {} along with text
         joke.push({ id: uuid(), text : response.data.joke , votes : 0 });
         this.setState({jokes : joke});
+        setTimeout(function(){
+            this.setState({isLoading: true});
+        }.bind(this),8000)
         }
     }
+    
+
+    
     handleVote(id , voting){
         this.setState(st => ({
             jokes : st.jokes.map(joke => 
-                // if joke id is equal to id it increases + voting(1 or -1) or return as it is
+                // if joke id is equal to id it increases + voting(1 or -1) otherwise not (joke) 
                 joke.id === id? {...joke, votes : joke.votes + voting} : joke 
                 )
         }))
@@ -46,7 +51,7 @@ class JokeCollection extends Component {
         return (
             <div className='bg-gray-200 h-full'>
                 <Header/>
-                {jokeData}
+                {this.state.isLoading? jokeData : <Loader/>}
                 <Footer/>
             </div>
         )
